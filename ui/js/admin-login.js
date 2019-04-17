cb16adb6cf44b8cdc29b2ea01cabc8e45e6b3275
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2019-04-17 22:11:46 +0100

    feature(client): create the loan repayments page
    create the page that shows the history of loan
    repayments the client has made
    
    [Finishes #165330870]

---
+++ ui/js/admin-login.js
@@ -0,0 +1,5 @@
+const adminLoginBtn = document.getElementById('admin-login-btn');
+
+adminLoginBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+});
--- ui/js/prevent-default.js
+++
@@ -1,6 +0,0 @@
-const form = document.getElementsByTagName('form')[0];
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    location = './loan-repayments.html';
-});
