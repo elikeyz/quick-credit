7a01534e15cdff155e76010f69453d880bc139ce
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2019-04-23 22:44:14 +0100

    feature(admin-ui): create the loans view
    create a tabbed view showing the loans in
    each category; All, Current and Repaid
    
    [Finishes #165331024, #165331031]

---
+++ ui/js/loans.js
@@ -0,0 +1,25 @@
+const allTab = document.getElementById('all-tab');
+const currentTab = document.getElementById('current-tab');
+const repaidTab = document.getElementById('repaid-tab');
+const tabs = [allTab, currentTab, repaidTab];
+
+allTab.addEventListener('click', () => {
+    tabs.forEach((tab) => {
+        tab.classList.remove('active-tab');
+    });
+    allTab.classList.add('active-tab');
+});
+
+currentTab.addEventListener('click', () => {
+    tabs.forEach((tab) => {
+        tab.classList.remove('active-tab');
+    });
+    currentTab.classList.add('active-tab');
+});
+
+repaidTab.addEventListener('click', () => {
+    tabs.forEach((tab) => {
+        tab.classList.remove('active-tab');
+    });
+    repaidTab.classList.add('active-tab');
+});
