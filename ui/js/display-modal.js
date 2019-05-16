e9d0030e12d9af11bcd73432c0f99b8776438e04
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2019-05-16 13:32:05 +0100

    refactor(modal): refactor the display modal function
    - refactor the display modal function by splitting it into two functions
    
    [Finishes #166058055]

--- ui/js/display-modal.js
+++ ui/js/display-modal.js
@@ -1,7 +1,8 @@
-const displayModal = (modalId, modalCancelBtnId, modalCloseBtnId, modalOpenBtnId, dataCount) => {
+const setModalBtnClickEvent = (modalOpenBtn, modalCancelBtnId, modalCloseBtnId, modalId) => {
   const modal = document.getElementById(modalId);
+  const modalBtn = document.getElementById(modalOpenBtn);
 
-  const openModal = (event) => {
+  modalBtn.addEventListener('click', (event) => {
     event.preventDefault();
     modal.style.display = 'flex';
 
@@ -15,19 +16,18 @@ const displayModal = (modalId, modalCancelBtnId, modalCloseBtnId, modalOpenBtnId
     closeBtn.addEventListener('click', () => {
       modal.style.display = 'none';
     });
-  };
+  });
+};
 
-  const setModalBtnClickEvent = (modalOpenBtn) => {
-    const modalBtn = document.getElementById(modalOpenBtn);
-    modalBtn.addEventListener('click', openModal);
-  };
+const displayModal = (modalId, modalCancelBtnId, modalCloseBtnId, modalOpenBtnId, dataCount) => {
+  const modal = document.getElementById(modalId);
 
   if (dataCount) {
     for (let loanId = 1; loanId <= dataCount; loanId += 1) {
-      setModalBtnClickEvent(`${modalOpenBtnId}-${loanId}`);
+      setModalBtnClickEvent(`${modalOpenBtnId}-${loanId}`, modalCancelBtnId, modalCloseBtnId, modalId);
     }
   } else {
-    setModalBtnClickEvent(modalOpenBtnId);
+    setModalBtnClickEvent(modalOpenBtnId, modalCancelBtnId, modalCloseBtnId, modalId);
   }
 
   window.addEventListener('click', (event) => {
