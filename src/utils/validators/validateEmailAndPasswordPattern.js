f6c36f0cf7dae542cf3218b146fed8f069cfcf63
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2019-05-19 15:14:52 +0100

    feature(users): setup the get my loans endpoint to use the database
    - set up the Get My Loans endpoint to fetch the user's loans from the
      database
    - set up a beforeEach hook to insert sample data into the test database
      for testing purposes
    
    [Finishes #166083690]

--- src/utils/validators/validateEmailAndPasswordPattern.js
+++ src/utils/validators/validateEmailAndPasswordPattern.js
@@ -1,6 +1,8 @@
 import sendErrorResponse from '../helpers/sendErrorResponse';
 
 const validateEmailAndPasswordPattern = (req, res, next) => {
+  // Email regex gotten from http://www.regexlib.com
+  // Whitespace regex gotten from https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/regular-expressions/match-whitespace
   if (!/^.+@[^.].*\.[a-z]{2,}$/.test(req.body.email.trim())) {
     sendErrorResponse(res, 400, 'Your email address must follow the pattern ****@**.**');
   } else if (/\s/g.test(req.body.password)) {
