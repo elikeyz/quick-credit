a5f8aa59d8c318eadc67ad18bc4862ff3e617716
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2019-05-29 23:56:55 +0100

    feature(client-ui): display client loan repayment history on ui
    - use fetch to get the loan repayment history of the logged in client and
      render it on the loan repayments history page on the client interface
    
    [Finishes #166339155]

---
+++ ui/js/fetch.js
@@ -0,0 +1,18 @@
+const { token } = JSON.parse(localStorage.getItem('quick-credit-access-data'));
+
+const apiGetFetch = url => fetch(`https://elikeyz-quick-credit.herokuapp.com/api/v1${url}`, {
+  method: 'get',
+  headers: {
+    'Content-type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  },
+}).then(response => response.json());
+
+const apiFetch = (url, method, body = {}) => fetch(`https://elikeyz-quick-credit.herokuapp.com/api/v1${url}`, {
+  headers: {
+    'Content-type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  },
+  body: JSON.stringify(body),
+  method,
+}).then(response => response.json());
