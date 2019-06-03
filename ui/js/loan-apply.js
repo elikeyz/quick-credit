e727665bf490a2f907a3ef1a104467fed79f85d5
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2019-06-03 20:20:45 +0100

    feature(client-ui): integrate the loan application feature
    - set a submit event on the loan application form to submit a loan
      application to the API.
    
    [Finishes #166396306]

--- ui/js/loan-apply.js
+++ ui/js/loan-apply.js
@@ -1,1 +1,32 @@
-displayModal('loan-application-form-modal', 'cancel-btn', 'close-btn', 'open-modal');
+const setupLoanApplicationForm = () => {
+  const form = document.getElementById('loan-application-form');
+  const submitBtn = document.getElementById('submit-btn');
+  const formFeedbackDiv = document.getElementById('loan-application-form-feedback');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    submitBtn.setAttribute('disabled', true);
+    formFeedbackDiv.innerHTML = '<p class="unverified">Sending loan application...</p>';
+    const requestBody = {
+      amount: Number(document.getElementById('amount').value),
+      purpose: document.getElementById('reason').value,
+      tenor: parseInt(document.getElementById('tenor').value, 10),
+    };
+    apiFetch('/loans', 'post', requestBody)
+      .then((data) => {
+        submitBtn.removeAttribute('disabled');
+        if (data.status === 201) {
+          formFeedbackDiv.innerHTML = '<p class="verified">Loan application sent successfully</p>';
+          location.reload();
+        } else if (data.status === 401) {
+          location.assign('./login.html');
+        } else {
+          formFeedbackDiv.innerHTML = `<p class="rejected">${data.error}</p>`;
+        }
+      })
+      .catch((err) => {
+        formFeedbackDiv.innerHTML = `<p class="rejected">${err}</p>`;
+      });
+  });
+};
+
+displayModal('loan-application-form-modal', 'cancel-btn', 'close-btn', 'open-modal', [], setupLoanApplicationForm);
